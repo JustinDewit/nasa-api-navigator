153ef06344ad935ded334b3c71ffb073d5f9c3b2
JustinDewit <137382666+JustinDewit@users.noreply.github.com>
2024-10-02 18:07:26 +0800

    fix(api): return error if rover is not provided in request

--- src/app/api/mars-photos/route.ts
+++ src/app/api/mars-photos/route.ts
@@ -2,7 +2,11 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const rover = searchParams.get('rover') || 'curiosity'; // Default to 'curiosity' if not provided
+  const rover = searchParams.get('rover');
+
+  if (!rover) {
+    return new NextResponse('A rover needs to be passed on', { status: 400 });
+  }
 
   const apiKey = process.env.NASA_API_KEY || 'DEMO_KEY'; // Use DEMO_KEY if NASA_API_KEY is not set
   const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${apiKey}`;
