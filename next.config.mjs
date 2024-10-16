509bf3891ab56b99cc45d217792ec509e5c2d8fa
JustinDewit <137382666+JustinDewit@users.noreply.github.com>
2024-10-16 17:02:47 +0800

    feat: add youtube domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['apod.nasa.gov', 'epic.gsfc.nasa.gov', 'mars.nasa.gov'],
+    domains: ['apod.nasa.gov', 'epic.gsfc.nasa.gov', 'mars.nasa.gov', 'www.youtube.com'],
   },
 };
 
